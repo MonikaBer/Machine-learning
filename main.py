eab7552ff121d0aae03e2e0e2afb73ca84512560
MonikaBer <berlinska.monika@outlook.com>
2020-01-11 21:05:47 +0100

    Testing of Bayes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import random
 from sklearn import svm
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 file = open("data_banknote_authentication.txt", "r")
 list = []
@@ -26,7 +29,8 @@ while i < len(list):
     y.append(list[i][4])
     i+=1
 
-average = []
+averageSVM = []
+averageBayes = []
 
 k = 10
 size = int(len(y)/k)
@@ -35,12 +39,12 @@ size = int(len(y)/k)
 testSet = 0
 while testSet < k:
     #testing SVM method using different test sets
-    clf = svm.SVC()
-    X = []
-    Y = []
+    clf = svm.SVC()      #SVM's constructor
+    gnb = GaussianNB()   #Bayes's constructor
+    X_train = []
+    Y_train = []
     for setNr in range(k):
         if setNr != testSet:
-
             i = setNr * size
             if setNr != 9:
                 finish = i + size
@@ -48,45 +52,57 @@ while testSet < k:
                 finish = len(y)
 
             while i < finish:
-                X.append(x[i])
-                Y.append(y[i])
+                X_train.append(x[i])
+                Y_train.append(y[i])
                 i+=1
 
             setNr+=1
 
-    #trening of SVM method using training set
-    clf.fit(X,Y)
-
-    #test prediction using test set
-    X = []
-    Y = []
+    X_test = []
+    Y_test = []
     i = testSet * size
     finish = i + size
     while i < finish:
-        X.append(x[i])
-        Y.append(y[i])
+        X_test.append(x[i])
+        Y_test.append(y[i])
         i+=1
 
-    resultOfPrediction = []
-    resultOfPrediction = clf.predict(X)
+    #training SVM and Bayes, and prediction
+    resultOfPredictionForSVM = []
+    clf.fit(X_train,Y_train)
+    resultOfPredictionForSVM = clf.predict(X_test)
 
-    numberOfHits = 0
+    resultOfPredictionForBayes = []
+    resultOfPredictionForBayes = gnb.fit(X_train, Y_train).predict(X_test)
+
+    numberOfHitsForSVM = 0
+    numberOfHitsForBayes = 0
     i = 0
-    while i < len(Y):
-        if resultOfPrediction[i] == Y[i]:
-            numberOfHits+=1
+    while i < len(Y_test):
+        if resultOfPredictionForSVM[i] == Y_test[i]:
+            numberOfHitsForSVM+=1
+        if resultOfPredictionForBayes[i] == Y_test[i]:
+            numberOfHitsForBayes+=1
         i+=1
 
-    result = numberOfHits/len(Y)
-    average.append(result)
+    resultForSVM = numberOfHitsForSVM/len(Y_test)
+    resultForBayes = numberOfHitsForBayes/len(Y_test)
+
+    averageSVM.append(resultForSVM)
+    averageBayes.append(resultForBayes)
 
     testSet+=1  #change of number of test set
 
-sum = 0
+sumForSVM = 0
+sumForBayes = 0
 i = 0
-while i < len(average):
-    sum += average[i]
+while i < len(averageSVM):
+    sumForSVM += averageSVM[i]
+    sumForBayes += averageBayes[i]
     i+=1
 
-resultForSVM = sum/len(average)
+resultForSVM = sumForSVM/len(averageSVM)
+resultForBayes = sumForBayes/len(averageBayes)
+
 print("Result for SVM", resultForSVM*100, "%")
+print("Result for Bayes", resultForBayes*100, "%")
